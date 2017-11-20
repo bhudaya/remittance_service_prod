73a0e6355b9a6f3560039f7c259fe6e74736fd24
amril <bhudaya@yahoo.com>
2017-11-20 11:55:26 +0700

    9250 update payment mode option

--- application/class/Iapps/RemittanceService/RemittanceRecord/RemittancePaymentModeOptionFactory.php
+++ application/class/Iapps/RemittanceService/RemittanceRecord/RemittancePaymentModeOptionFactory.php
@@ -9,6 +9,10 @@ use Iapps\RemittanceService\RemittanceRecord\Mode\DefaultPaymentModeOption;
 use Iapps\RemittanceService\RemittanceRecord\Mode\GPLPaymentModeOption;
 use Iapps\RemittanceService\RemittanceTransaction\RemittanceTransaction;
 use Iapps\RemittanceService\RemittanceRecord\Mode\TMoneyPaymentModeOption;
+use Iapps\RemittanceService\RemittanceRecord\Mode\TransferToPaymentModeOption;
+use Iapps\RemittanceService\RemittanceRecord\Mode\TransferToPaymentModeOption3;
+use Iapps\RemittanceService\RemittanceRecord\Mode\TransferToCashPickupPaymentModeOption;
+
 
 
 
@@ -18,6 +22,11 @@ class RemittancePaymentModeOptionFactory{
     const EWALLET_PAYMENT_CODE = 'EWA';
     const BANK_TRANSFER_GPL = 'BT8';
     const TMONEY_PAYMENT_CODE = 'BT7';
+    const TRANSFERTO_PAYMENT_CODE = 'TT1';
+    const TRANSFERTO_PAYMENT_CODE_3 = 'TT3';
+    const TRANSFERTO_CASH_PICKUP_PAYMENT_CODE = 'CP2';
+
+
 
 
     public static function build(RemittanceTransaction $transaction, $sender_id, $recipient_id, $ipAddress='127.0.0.1', $updatedBy=NULL)
@@ -31,6 +40,15 @@ class RemittancePaymentModeOptionFactory{
                 break;
             case self::TMONEY_PAYMENT_CODE:
                 self::$_instance = new TMoneyPaymentModeOption($transaction, $sender_id, $recipient_id);
+                break;            
+            case self::TRANSFERTO_PAYMENT_CODE:
+                self::$_instance = new TransferToPaymentModeOption($transaction, $sender_id, $recipient_id);
+                break;
+            case self::TRANSFERTO_PAYMENT_CODE_3:
+                self::$_instance = new TransferToPaymentModeOption3($transaction, $sender_id, $recipient_id);
+                break;
+            case self::TRANSFERTO_CASH_PICKUP_PAYMENT_CODE:
+                self::$_instance = new TransferToCashPickupPaymentModeOption($transaction, $sender_id, $recipient_id);
                 break;
             default:
                 self::$_instance = new DefaultPaymentModeOption($transaction, $sender_id, $recipient_id);
